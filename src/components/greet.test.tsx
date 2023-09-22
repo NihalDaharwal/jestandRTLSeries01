c0473feb3610b272808690f96d293a725bed6899
nihaldaharwal <nsanihaldaharawal@gmail.com>
2023-09-22 13:36:02 +0530

    greet component test file added

---
+++ src/components/greet.test.tsx
@@ -0,0 +1,19 @@
+//Greet should render the test hello and if name is passed in to the component
+//it should render followed by the name
+
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
+
+test("greet rendered correctly", () => {
+  render(<Greet />);
+  //   const textElement = screen.getByText("Hello");
+  const textElement = screen.getByText(/Hello/);
+  //   const textElement = screen.getByText(/hello/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test("greet render with a name", () => {
+  render(<Greet name="Nihal" />);
+  const textElement = screen.getByText("Hello Nihal");
+  expect(textElement).toBeInTheDocument();
+});
