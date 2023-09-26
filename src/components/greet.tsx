e29d9173e6cf28c957adee162872794e8437c5e6
nihaldaharwal <nsanihaldaharawal@gmail.com>
2023-09-26 14:51:50 +0530

    assets and section test added

--- src/components/greet.tsx
+++ src/components/greet.tsx
@@ -1,7 +1,9 @@
-type GreetProps = {
-  name?: string;
-};
+import { GreetProps } from "./greet.type";
+// type GreetProps = {
+//   name?: string;
+// };
 
 export const Greet = (props: GreetProps) => {
-  return <div>Hello {props.name}</div>;
+  // return <div>Hello {props.name}</div>;
+  return <div>Hello {props.name ? props.name : "Guest"}</div>;
 };
