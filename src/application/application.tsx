27dc63df2bbefbe174f03512d8f129672034a7f5
nihaldaharwal <nsanihaldaharawal@gmail.com>
2023-09-28 15:33:05 +0530

    Revert "Revert "files: getByRole,role option labeltext,placeholdertex,getBytext added""
    This reverts commit 046cdf44bc46984bd4091947da9ce5281965b900.

--- src/application/application.tsx
+++ src/application/application.tsx
@@ -1,13 +1,13 @@
 export const Application = () => {
   return (
     <>
-      {/* <h1>Job application form</h1>
+      <h1>Job application form</h1>
       <h2>Section 1</h2>
       <p>All fields are mandatory</p>
-      <span title="close">X</span>
-      <img src="https://via.placeholder.com/150" alt="a person with a laptop" />
+      {/* <span title="close">X</span>
+      <img src="https://via.placeholder.com/150" alt="a person with a laptop" /> */}
 
-      <div data-testid="custom-element">Custom HTML element</div> */}
+      <div data-testid="custom-element">Custom HTML element</div>
 
       <form>
         <div>
@@ -15,17 +15,16 @@ export const Application = () => {
           <input
             type="text"
             id="name"
-            // placeholder="Fullname"
+            placeholder="Fullname"
             // value="Nihal"
             // onChange={() => {}}
           />
         </div>
-        
-        {/* 
+
         <div>
           <label htmlFor="bio">Bio</label>
           <textarea id="bio" />
-        </div> */}
+        </div>
 
         <div>
           <label htmlFor="job-location">Job location</label>
